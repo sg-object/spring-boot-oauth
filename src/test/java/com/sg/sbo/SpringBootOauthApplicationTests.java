d4a8484e0d74e2132d7da5184ea991c148834eb6
sg.object <sg.object@gmail.com>
2023-05-29 09:52:18 +0900

    first commit

---
+++ src/test/java/com/sg/sbo/SpringBootOauthApplicationTests.java
@@ -0,0 +1,13 @@
+package com.sg.sbo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringBootOauthApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
